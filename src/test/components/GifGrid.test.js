6646c3b01a48b2fed5b297ba2bcc9c16b991349c
alvaro-78 <alvaro78an@gmail.com>
2021-11-05 18:01:04 +0100

    Add GifGrid test

--- src/test/components/GifGrid.test.js
+++ src/test/components/GifGrid.test.js
@@ -5,6 +5,7 @@ import useFetchGifs from '../../hooks/useFetchGifs';
 jest.mock('../../hooks/useFetchGifs');
 
 describe('test GifGrid component', () => {
+    // Mock data
     useFetchGifs.mockReturnValue({
         data: [],
         loading: true,
@@ -13,26 +14,35 @@ describe('test GifGrid component', () => {
     const category = 'random';
     const wrapper = shallow(<GifGrid category={category} />);
 
+    // tes component
     it('should render component correctly', () => {
         expect(wrapper).toMatchSnapshot();
     });
 
-    it('should show items when images are loaded useFetchGifs  ', () => {
+    // test our custom Hook return mock data
+    it('should show items when images are loaded useFetchGifs', () => {
         const gifs = [
             {
                 id: 'abc',
                 url: 'https://localhost/loquesea/cosa.jpg',
                 title: 'Cualquiercosa',
             },
+            {
+                id: 'def',
+                url: 'https://localhost/loquesea/otracosa.jpg',
+                title: 'Cualquier otra cosa',
+            },
         ];
 
         useFetchGifs.mockReturnValue({
             data: gifs,
-            loading: true,
+            loading: false,
         });
 
         const wrapper = shallow(<GifGrid category={category} />);
 
         expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('p').exists()).toBe(false);
+        expect(wrapper.find('GifGridItem').length).toBe(gifs.length);
     });
 });
--- src/test/helpers/getGifs.test.js
+++ src/test/helpers/getGifs.test.js
@@ -2,8 +2,8 @@ import getGifs from '../../helpers/getGifs';
 
 // test asynchronous
 describe('Fetch test', () => {
-    it('should had ten elements', async () => {
-        const gifs = await getGifs('category');
+    it('should had twenty elements', async () => {
+        const gifs = await getGifs('random');
 
         expect(gifs.length).toBe(20);
     });
