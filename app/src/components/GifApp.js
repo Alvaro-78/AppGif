bf23c414995b896c7214f329d4497feb764f9cb4
alvaro-78 <alvaro78an@gmail.com>
2021-11-05 19:27:17 +0100

    Update directory

--- app/src/components/GifApp.js
+++ app/src/components/GifApp.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import AddCategory from '../components/AddCategory';
+import AddCategory from './AddCategory';
 import GifGrid from './GifGrid';
 
 const GifApp = () => {
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import GifApp from '../src/components/GifApp'
-
-import './index.css'
-
-ReactDOM.render(<GifApp />, document.getElementById('root'))
