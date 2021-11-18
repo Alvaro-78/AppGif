edf93216c1e205909840e9352d3cfab854e55132
alvaro-78 <alvaro78an@gmail.com>
2021-11-18 13:00:59 +0100

    Add useFetchGifs test

--- app/src/test/hooks/useFetchGif.test.js
+++ app/src/test/hooks/useFetchGif.test.js
@@ -1,13 +1,24 @@
 import useFetchGifs from '../../hooks/useFetchGifs';
 import { renderHook } from '@testing-library/react-hooks';
 
-describe('Testing custom Hook useFetchGifs', () => {
-    // test what should return our custom hook
-    it('return initial state', () => {
+describe('Pruebas en el hook useFetchGifs', () => {
+    test('debe de retornar el estado inicial', () => {
         const { result } = renderHook(() => useFetchGifs('random'));
+
         const { data, loading } = result.current;
 
         expect(data).toEqual([]);
-        expect(loading).toEqual(true);
+        expect(loading).toBe(true);
+    });
+
+    test('debe de retornar un arreglo de imgs y el loading en false', async () => {
+        const { result, waitForNextUpdate } = renderHook(() =>
+            useFetchGifs('random'),
+        );
+        await waitForNextUpdate();
+        const { data, loading } = result.current;
+
+        expect(data.length).toBe(20);
+        expect(loading).toBe(false);
     });
 });
